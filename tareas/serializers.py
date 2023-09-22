4503afac6a60906bef5d03985ca63c4dc9d04b98
orlandoc0107 <orlandocardenas0107@gmail.com>
2023-09-22 19:55:32 -0300

    update

--- tareas/serializers.py
+++ tareas/serializers.py
@@ -27,13 +27,13 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'password', 'email', 'first_name', 'last_name')
-
-    def create(self, validated_data):
-        user = User.objects.create_user(
+        
+        def create(self, validated_data):
+            user = User.objects.create_user(
             username=validated_data['username'],
             password=validated_data['password'],
-            #email=validated_data['email'],
-            first_name=validated_data.get('first_name', ''),
-            last_name=validated_data.get('last_name', ''),
-        )
-        return user
+            email=validated_data['email'],
+            first_name=validated_data.get('first_name'),  
+            last_name=validated_data.get('last_name')  
+            )
+            return user
--- tareas/urls.py
+++ tareas/urls.py
@@ -5,12 +5,6 @@ from django.contrib import admin
 from tareas import views
 from tareas.views import MostarTareas, CrearTarea, BorrarTarea, ActualizarTarea, DetallesTarea
 from tareas.views import Login, Registro
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
-
 
 
 router = routers.DefaultRouter()
@@ -26,8 +20,4 @@ urlpatterns = [
        path('actualiza-tarea/<int:pk>/', ActualizarTarea.as_view()), # PUT / pk= es id de la tarea en la url
        path('registro/', Registro.as_view(), name='register'), # POST
        path('ingresar/', Login.as_view()), #POST
-       #path('datos/',Datos_user.as_view()),
-       path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # POST , esto es igual que el Login
-       path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), #POST ,
-       path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'), #POST ,
 ]
